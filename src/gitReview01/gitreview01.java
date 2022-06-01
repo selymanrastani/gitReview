07648fa2465b3e8035583a17eb2d74780d41cf69
selymanrastani <selyman.rastani@gmail.com>
2022-05-31 21:35:43 -0400

    1st commit

---
+++ src/gitReview01/gitreview01.java
@@ -0,0 +1,12 @@
+package gitReview01;
+
+public class gitreview01 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Today we learn github for practic");
+		System.out.println("It is a good day to practic");
+
+	}
+
+}
